8c014e4204c806e8bc69768847096188820771d0
2012035 <114689396+2012035@users.noreply.github.com>
2023-05-09 18:49:18 +0530

    Static Warp_perspective

---
+++ wrap_prespective.py
@@ -0,0 +1,19 @@
+import numpy as np;
+import cv2 as c;
+image=c.imread('image/demo.jpeg');
+width,height=480,540;
+
+
+point1=np.float32([[12,229],[16,870],[452,241],[452,844]]);
+point2=np.float32([[0,0],[0,width],[height,0],[height,width]]);
+
+#join the points with original width and heigth
+matrix=c.getPerspectiveTransform(point1,point2)
+image1=c.warpPerspective(image,matrix,(width,height))
+
+
+
+c.imshow("Demo",image1)
+while True:
+    if c.waitKey(1) & 0xFF == ord('z'):
+     break;
