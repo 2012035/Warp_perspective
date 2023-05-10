9cb0ededd5a48df9dfe958635f5f07e68311ea13
2012035 <114689396+2012035@users.noreply.github.com>
2023-05-10 20:45:38 +0530

    Update wrap_prespective.py

--- wrap_prespective.py
+++ wrap_prespective.py
@@ -1,7 +1,7 @@
 import numpy as np;
 import cv2 as c;
 image=c.imread('image/demo.jpeg');
-width,height=480,540;
+width,height=490,510;
 
 
 point1=np.float32([[12,229],[16,870],[452,241],[452,844]]);
@@ -10,7 +10,6 @@ point2=np.float32([[0,0],[0,width],[height,0],[height,width]]);
 #join the points with original width and heigth
 matrix=c.getPerspectiveTransform(point1,point2)
 image1=c.warpPerspective(image,matrix,(width,height))
-print('hello')
 
 
 
